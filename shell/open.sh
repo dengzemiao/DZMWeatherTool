ed5a5c6f8db01ddd27c7bea66e3e9dd0e50cd22c
dengzemiao <dengzemiaovip@163.com>
2024-05-11 09:42:17 +0800

    2024-05-11 09:42:14 提交优化

--- shell/open.sh
+++ shell/open.sh
@@ -8,14 +8,14 @@ open "$cpath/天气模版.txt"
 # 头条地址
 open https://mp.toutiao.com/profile_v4/graphic/articles
 
-# 智普清言（AI）
-open https://chatglm.cn/main/detail
-
 # 天气数据图（降水量、气温预报、大风实测等）
 open http://wx.nmc.cn/f
 
 # 全国天气预报（搜索城市，查看未来7-40天天气）
 open http://www.weather.com.cn/forecast/
+
+# 智普清言（AI）
+open https://chatglm.cn/main/detail
 
 # 天气工具（现地址）
 # open http://10.0.51.147:8080/hefeng/weather
